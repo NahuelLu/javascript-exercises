29023128ccfc92cbbf66bb443d28784a791dbee2
NahuelLu <nahuellucana1999@gmail.com>
2022-06-01 13:20:36 -0300

    Completed fibonacci exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
-
+'use strict'
+const fibonacci = function(number) {
+    if(number < 0)
+        return "OOPS";
+    let array = [0,1];
+    while(array.length <= number){
+        array.push(array[array.length-1]+array[array.length-2]);
+    };
+    return array[array.length - 1];
 };
 
 // Do not edit below this line
