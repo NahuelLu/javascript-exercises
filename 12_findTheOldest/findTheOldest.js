69655730952cfee1f22ce53956bef8da65843604
NahuelLu <nahuellucana1999@gmail.com>
2022-06-01 18:34:54 -0300

    Completed getTheTitles and findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson,person) => {
+        if(oldestPerson===null)
+            return oldestPerson;
+        let yearLived1= getAge(oldestPerson.yearOfBirth,oldestPerson.yearOfDeath);
+        let yearLived2= getAge(person.yearOfBirth,person.yearOfDeath);
+        return yearLived1 > yearLived2? oldestPerson : person;
+    },{})
 };
+function getAge(year1, year2){
+    if(!year2){
+        year2= new Date().getFullYear();
+    }
+    return year2 - year1;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
