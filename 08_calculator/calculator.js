40272a3907f36ef62d0f07a63b07997bb3b55fbf
NahuelLu <nahuellucana1999@gmail.com>
2022-06-01 00:52:16 -0300

    Completed calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+'use strict' 
+const add = function(num1,num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	return numbers.reduce((totalSum,number)=> {
+    return totalSum + number;
+  },0);
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce((total,number)=>{
+    return total * number;
+  },1);
 };
 
-const power = function() {
-	
+const power = function(number,power) {
+  let total=1;
+  for(let i=1;i<=power;i++){
+    total*=number;
+  }
+  return total; 
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  let total=1;
+	for(let i = number;i>0;i--){
+      total*=i;
+  }
+  return total;
 };
 
 // Do not edit below this line
