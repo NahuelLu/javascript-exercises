6a294725d1d2b18533f3a4f804d5454cf2ac43fc
NahuelLu <nahuellucana1999@gmail.com>
2022-06-01 12:17:52 -0300

    Completed palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+'use strict'
+const palindromes = function (word) {
+    const wordClean = word
+        .toLowerCase().replace(/[^A-Za-z]/g, "")
+    return  [...wordClean].reverse().join("")=== wordClean;
 };
 
 // Do not edit below this line
